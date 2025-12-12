a00a78cf120f8fc89ac634a823afc24a1c8b3842
Jhondrek <jhondrek116@gmail.com>
2025-12-12 13:36:40 -0600

    anchors urls updates for linkeding in help.jsx

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -14,9 +14,9 @@ export default function Help(){
                 <p className="hero__tittle">Feel free to contact me through any of the following methods: </p>
 
                 <section className="contactCont__linkSectionCont">
-                    <a className="btn" href="mailto:recipient@example.com">Send an Email</a>
-                    <a  className="btn" href="mailto:recipient@example.com">Contact through Linkedin</a>
-                    <a  className="btn" href="https://wa.me/+50670340543">Send a Whatsapp</a>
+                    <a className="btn" target="_blank" href="mailto:jhondrek116@gmail.com">Send an Email</a>
+                    <a  className="btn" target="_blank" href="https://www.linkedin.com/in/david-fern%C3%A1ndez-0b103611a/">Contact through Linkedin</a>
+                    <a  className="btn" target="_blank" href="https://wa.me/+50670340543">Send a Whatsapp</a>
                 </section>
             </main>
     )
